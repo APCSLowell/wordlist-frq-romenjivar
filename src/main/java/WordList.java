2d438764bd8acdccf68cfba490e274d2d17d49fe
romenjivar <162660537+romenjivar@users.noreply.github.com>
2025-04-09 12:13:34 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,22 +12,21 @@ public class WordList
   {
     /* to be implemented in part (a) */
     int count = 0;
-    for(int i = 0; i < myList.size(); i++){
-        if(myList.get(i).length() == len){
+    for(int i = 0; i < myList.size(); i++)
+        if(myList.get(i).length() == len)
           count++;
-        }
-    }
+        
     return count;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-    for(int i = 0; i < myList.size(); i++){
+    for(int i = 0; i < myList.size(); i++)
       if(myList.get(i).length() == len){
         myList.remove(i);
         i--;
-      }
+      
     }
     return myList;
   }
